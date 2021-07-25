3cde6447cda1b83eb1a78e9f4c1c6ab324487145
WyattHWilliams <whwilliams.library@gmail.com>
2021-07-25 13:49:38 -1000

    added logout button to user info

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { LogOut } from '../actions/profileActionCreators';
 
 function UserInfo({ user }) {
+    let history = useHistory();
+    let dispatch = useDispatch();
+
+    async function logout() {
+        try {
+            let res = await dispatch(LogOut());
+            if (res === 'logout failure') throw new Error(res);
+            if (res === 'logout success') history.push('/home');
+        } catch (err) {
+            alert(err);
+            console.log(err.stack);
+        }
+    }
+
     return (
         <div className="UserInfo-Component">
             <h3>{user.firstName} {user.lastName}</h3>
             <p>email: {user.email}</p>
             <p>admin: {user.isAdmin === true ? 'true' : 'false'}</p>
             <p>status: {user.state}</p>
+            <button onClick={() => logout()}>logout</button>
         </div>
     )
 }
